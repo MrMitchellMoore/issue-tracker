95b61685197d6dab41ef7497d001f16a5e899e94
Mitchell Moore <mmoorejr24@gmail.com>
2023-10-30 16:27:14 -0700

    Handle Form Submission

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -3,13 +3,47 @@
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import { Pencil } from "lucide-react";
 import React from "react";
+import { createIssueSchema } from "@/../prisma/Schemas";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import clsx from "clsx";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-type Props = {};
+const schema = createIssueSchema;
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center z-[100]">
-      <div className="border-2 rounded-md border-gray-500 flex flex-col mx-auto p-14 md:py-20 md:px-14 w-screen-xl sm:w-3/4">
+      <div
+        className={clsx({
+          "w-full bg-pink-700 text-black": errors,
+        })}
+      >
+        {errors.root?.message}
+      </div>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            const postData = await axios.post("/api/issues", data);
+            if (!postData.data) {
+              console.log("Something went wrong!");
+            }
+            router.push("/issues");
+          } catch (error) {
+            console.log("Something went wrong in the process " + error);
+          }
+        })}
+        className="border-2 rounded-md border-gray-500 flex flex-col mx-auto p-14 md:py-20 md:px-14 w-screen-xl sm:w-3/4"
+      >
         <h1 className="py-5 text-4xl font-semibold justify-center items-center">
           Create New Issue
         </h1>
@@ -19,17 +53,30 @@ const NewIssuePage = () => {
               <TextField.Slot>
                 <Pencil size={18} color="black" />
               </TextField.Slot>
-              <TextField.Input placeholder="Title" size="3" />
+              <TextField.Input
+                placeholder="Title"
+                size="3"
+                {...register("title")}
+              />
             </TextField.Root>
-            <TextArea size="3" placeholder="Description" />
+            <TextArea
+              size="3"
+              placeholder="Description"
+              {...register("description")}
+            />
           </div>
           <div>
-            <Button radius="medium" size="2" style={{ cursor: "pointer" }}>
+            <Button
+              radius="medium"
+              size="2"
+              style={{ cursor: "pointer" }}
+              color="blue"
+            >
               New Issue
             </Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
