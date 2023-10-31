37e051c95856c2135ca71e9302281a06c760c061
Mitchell Moore <mmoorejr24@gmail.com>
2023-10-30 17:44:44 -0700

    Implement client-side validation

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -2,46 +2,51 @@
 
 import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import { Info, Pencil } from "lucide-react";
-import React, { useState } from "react";
 import { createIssueSchema } from "@/../prisma/Schemas";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import clsx from "clsx";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-const schema = createIssueSchema;
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, handleSubmit, reset } = useForm({
-    resolver: zodResolver(schema),
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
-  const [errors, setErrors] = useState("");
+
   return (
-    <>
-      {errors && (
+    <div className="w-full h-screen flex flex-col justify-center items-center z-[100]">
+      {errors.title?.message || errors.description?.message ? (
         <Callout.Root size="3">
           <Callout.Icon>
             <Info color="black" />
           </Callout.Icon>
-          <Callout.Text>{errors}</Callout.Text>
+          <Callout.Text>
+            {errors.title?.message || errors.description?.message}
+          </Callout.Text>
         </Callout.Root>
+      ) : (
+        ""
       )}
-
       <form
+        className="w-full flex my-2"
         onSubmit={handleSubmit(async (data) => {
           try {
             await axios.post("/api/issues", data);
             reset();
             router.push("/issues");
-          } catch (error) {
-            console.log(
-              "Something went wrong in the process " + setErrors(errors)
-            );
+          } catch (error: any) {
+            console.log("Something went wrong in the process " + errors);
           }
         })}
-        className="w-full h-screen flex flex-col justify-center items-center z-[100]"
       >
         <div className="border-2 rounded-md border-gray-500 flex flex-col mx-auto p-14 md:py-20 md:px-14 w-screen-xl sm:w-3/4">
           <h1 className="py-5 text-4xl font-semibold justify-center items-center">
@@ -78,7 +83,7 @@ const NewIssuePage = () => {
           </div>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
